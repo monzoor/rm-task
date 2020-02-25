40f759143c1c1751c6e7b382d57bcba633109d9e
Monzoor Morshed Mahmud <mmtamal@gmail.com>
2020-02-25 22:07:16 +0600

    Remove dropzone

--- client/src/Components/Dashboard/CreateContent.js
+++ client/src/Components/Dashboard/CreateContent.js
@@ -13,37 +13,7 @@ import {
     TextInput,
 } from 'react-bootstrap4-form-validation';
 import { post } from 'axios';
-import Dropzone from 'react-dropzone-uploader';
-import '../../../node_modules/react-dropzone-uploader/dist/styles.css';
 
-const MyUploader = () => {
-    // specify upload params and url for your files
-    const getUploadParams = ({ meta }) => {
-        console.log('----', meta);
-
-        return { url: 'https://httpbin.org/post' };
-    };
-
-    // called every time a file's `status` changes
-    const handleChangeStatus = ({ meta, file }, status) => {
-        // console.log(status, meta, file);
-    };
-
-    // receives array of files that are done uploading when submit button is clicked
-    const handleSubmit = (files, allFiles) => {
-        // console.log(files.map(f => f.meta));
-        allFiles.forEach(f => f.remove());
-    };
-
-    return (
-        <Dropzone
-            getUploadParams={getUploadParams}
-            onChangeStatus={handleChangeStatus}
-            onSubmit={handleSubmit}
-            accept="image/*,audio/*,video/*"
-        />
-    );
-};
 const CreateContent = () => {
     const alertPopUp = useAlert();
     const [loadingData, setLoadingData] = useState(false);
